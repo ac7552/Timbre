e33de982813ebe1ae9982ade1542ce3a1bdd3441
Aaron Campbell <aaroncampbell@Aarons-MacBook-Pro.local>
2016-10-25 17:50:38 -0700

    new add:

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const splash = () => (
   <div className="logoContainer">
-    <h1 className="websiteLogo">Timbre.</h1>
+    <h1 className="websiteLogo">Timbre</h1>
   </div>
 )
 
